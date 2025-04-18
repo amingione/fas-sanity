249ec34cdd1165aeb8ada75e88cee1cb6a8ccdc7
Amber Mingione <ambermingione@gmail.com>
2025-04-17 23:23:25 -0400

    refactor: replace 'wooProduct' references with 'product' in customer, product, and productBundle schemas

--- schemaTypes/documents/productBundle.ts
+++ schemaTypes/documents/productBundle.ts
@@ -24,7 +24,7 @@ export const productBundle = defineType({
       name: 'products',
       title: 'Products in this Bundle',
       type: 'array',
-      of: [{ type: 'reference', to: [{ type: 'wooProduct' }] }]
+      of: [{ type: 'reference', to: [{ type: 'product' }] }]
     }),
     defineField({
       name: 'bundlePrice',
