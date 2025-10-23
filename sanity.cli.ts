a732d654de4e09e22f109b903372a2b2babbae9e
Amber Mingione <ambermingione@gmail.com>
2025-10-23 14:40:18 -0400

    feat: update pnpm workspace ignored dependencies and enhance Sanity CLI configuration
    - Added 'cpu-features' and 'ssh2' to ignored built dependencies in pnpm-workspace.yaml.
    - Enhanced Sanity CLI configuration with server settings and GraphQL options.
    - Updated appId in Sanity CLI configuration.
    - Integrated presentation tool into Sanity configuration with dynamic preview origin.
    - Added user.json file to define project user roles and permissions.

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,8 +3,21 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'r4og35qd',
-    dataset: 'production'
+    dataset: 'production',
   },
+  server: {
+    hostname: 'localhost',
+    port: 3333,
+  },
+  graphql: [
+    {
+      tag: 'default',
+      playground: true,
+      generation: 'gen3',
+      nonNullDocumentFields: false,
+    },
+  ],
+  vite: (config) => config,
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
@@ -20,6 +33,6 @@ export default defineCliConfig({
      * the package manager.
      */
     autoUpdates: false,
-    appId: 'drquzbmyskyqp32jncsii7os',
+    appId: 'kms7ld14hoja977u3tt9hc8z',
   },
 })
